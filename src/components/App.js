5e6b41a2e4c68f2b8930108ca2d08dc02c0190d5
Jens van Hellemondt <jens.vanhellemondt@gmail.com>
2020-07-28 13:22:05 +0200

    Href fix

--- src/components/App.js
+++ src/components/App.js
@@ -7,26 +7,26 @@ const App = () => {
     <div>
       <div class='ui large top fixed hidden menu'>
         <div class='ui container'>
-          <a href='#' class='active item'>
+          <a href='./' class='active item'>
             Home
           </a>
-          <a href='#' class='item'>
+          <a href='./' class='item'>
             Work
           </a>
-          <a href='#' class='item'>
+          <a href='./' class='item'>
             Company
           </a>
-          <a href='#' class='item'>
+          <a href='./' class='item'>
             Careers
           </a>
           <div class='right menu'>
             <div class='item'>
-              <a href='#' class='ui button'>
+              <a href='./' class='ui button'>
                 Log in
               </a>
             </div>
             <div class='item'>
-              <a href='#' class='ui primary button'>
+              <a href='./' class='ui primary button'>
                 Sign Up
               </a>
             </div>
@@ -35,22 +35,22 @@ const App = () => {
       </div>
 
       <div class='ui vertical inverted sidebar menu'>
-        <a href='#' class='active item'>
+        <a href='./' class='active item'>
           Home
         </a>
-        <a href='#' class='item'>
+        <a href='./' class='item'>
           Work
         </a>
-        <a href='#' class='item'>
+        <a href='./' class='item'>
           Company
         </a>
-        <a href='#' class='item'>
+        <a href='./' class='item'>
           Careers
         </a>
-        <a href='#' class='item'>
+        <a href='./' class='item'>
           Login
         </a>
-        <a href='#' class='item'>
+        <a href='./' class='item'>
           Signup
         </a>
       </div>
@@ -59,26 +59,26 @@ const App = () => {
         <div class='ui inverted vertical masthead center aligned segment'>
           <div class='ui container'>
             <div class='ui large secondary inverted pointing menu'>
-              <a href='#' class='toc item'>
+              <a href='./' class='toc item'>
                 <i class='sidebar icon'></i>
               </a>
-              <a href='#' class='active item'>
+              <a href='./' class='active item'>
                 Home
               </a>
-              <a href='#' class='item'>
+              <a href='./' class='item'>
                 Work
               </a>
-              <a href='#' class='item'>
+              <a href='./' class='item'>
                 Company
               </a>
-              <a href='#' class='item'>
+              <a href='./' class='item'>
                 Careers
               </a>
               <div class='right item'>
-                <a href='#' class='ui inverted button'>
+                <a href='./' class='ui inverted button'>
                   Log in
                 </a>
-                <a href='#' class='ui inverted button'>
+                <a href='./' class='ui inverted button'>
                   Sign Up
                 </a>
               </div>
@@ -103,16 +103,16 @@ const App = () => {
               <div class='three wide column'>
                 <h4 class='ui inverted header'>About</h4>
                 <div class='ui inverted link list'>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     Sitemap
                   </a>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     Contact Us
                   </a>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     Religious Ceremonies
                   </a>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     Gazebo Plans
                   </a>
                 </div>
@@ -120,16 +120,16 @@ const App = () => {
               <div class='three wide column'>
                 <h4 class='ui inverted header'>Services</h4>
                 <div class='ui inverted link list'>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     Banana Pre-Order
                   </a>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     DNA FAQ
                   </a>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     How To Access
                   </a>
-                  <a href='#' class='item'>
+                  <a href='./' class='item'>
                     Favorite X-Men
                   </a>
                 </div>
